0f89e8526fb6d594d8c8aaa55d3c05481a5b18b0
zrp8858 <zacharypatterson789@gmail.com>
2026-04-21 11:54:52 -0400

    refactor: using numpy for the rendering now - massive speedup

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
-from PyQt5.QtCore import QPoint, QRect, QTimer
-from PyQt5.QtGui import QImage, QPainter, qRgb
+import numpy as np
+from PyQt5.QtCore import QTimer
+from PyQt5.QtGui import QImage, QPainter
 from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget
 
 
@@ -8,32 +9,32 @@ class SandGrid:
     def __init__(self, width, height):
         self.width = width
         self.height = height
-        self.grid = [[0 for _ in range(width)] for _ in range(height)]
+        self.grid = np.zeros((height, width), dtype=np.uint8)
 
         # spawn line at top
         for x in range(width):
-            self.grid[0][x] = 1
+            self.grid[0, x] = 1
 
     def update_grid(self):
         for y in range(self.height - 2, -1, -1):
             for x in range(self.width):
-                if self.grid[y][x] == 1:
+                if self.grid[y, x] == 1:
                     self.update_pixel(x, y)
 
     def update_pixel(self, x, y):
-        if y + 1 < self.height and self.grid[y + 1][x] == 0:
-            self.grid[y][x] = 0
-            self.grid[y + 1][x] = 1
+        if y + 1 < self.height and self.grid[y + 1, x] == 0:
+            self.grid[y, x] = 0
+            self.grid[y + 1, x] = 1
 
-        elif x > 0 and y + 1 < self.height and self.grid[y + 1][x - 1] == 0:
-            self.grid[y][x] = 0
-            self.grid[y + 1][x - 1] = 1
+        elif x > 0 and y + 1 < self.height and self.grid[y + 1, x - 1] == 0:
+            self.grid[y, x] = 0
+            self.grid[y + 1, x - 1] = 1
 
         elif (
-            x < self.width - 1 and y + 1 < self.height and self.grid[y + 1][x + 1] == 0
+            x < self.width - 1 and y + 1 < self.height and self.grid[y + 1, x + 1] == 0
         ):
-            self.grid[y][x] = 0
-            self.grid[y + 1][x + 1] = 1
+            self.grid[y, x] = 0
+            self.grid[y + 1, x + 1] = 1
 
 
 class SandWidget(QWidget):
@@ -52,14 +53,19 @@ class SandWidget(QWidget):
 
     def paintEvent(self, event):
         painter = QPainter(self)
-        image = QImage(self.width, self.sand_grid.height, QImage.Format_RGB32)
 
-        for y in range(self.height):
-            for x in range(self.width):
-                if self.sand_grid.grid[y][x] == 1:
-                    image.setPixel(x, y, qRgb(189, 149, 3))
-                else:
-                    image.setPixel(x, y, qRgb(30, 30, 30))
+        rgb = np.zeros((self.height, self.width, 3), dtype=np.uint8)
+
+        rgb[self.sand_grid.grid == 1] = [189, 149, 3]
+        rgb[self.sand_grid.grid == 0] = [30, 30, 30]
+
+        image = QImage(
+            rgb.data,
+            self.width,
+            self.height,
+            3 * self.width,
+            QImage.Format_RGB888
+        )
 
         painter.drawImage(self.rect(), image)
 
@@ -73,7 +79,7 @@ class MainWindow(QMainWindow):
         self.setGeometry(0, 0, 2000, 2000)
 
         # Set up the sand widget
-        self.sand_widget = SandWidget(500, 500)
+        self.sand_widget = SandWidget(50, 50)
         self.setCentralWidget(self.sand_widget)
 
 
